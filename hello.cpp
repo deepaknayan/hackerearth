d7fcffd91560a6276b606d8a099e446b52c4da8c
deepaknayan <deepaknayanpro@gmail.com>
2018-07-04 19:59:16 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);			// for getting input from input.txt
+    freopen("output.txt", "w", stdout);			// for writing output to output.txt
+	#endif
+	cout << "Hello world" << endl;
+}
